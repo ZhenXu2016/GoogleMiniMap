4256d187cd7fc9f53665c4e63f5a00972e719933
ZhenXu2016 <Zhen.Xu2016@gmail.com>
2017-07-04 05:55:23 +0000

    Do nearby research on backend

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const path = require('path');
 const express = require('express');
+const request = require('request');
 const app = express();
 
 const PORT_NUMBER = 8080;
+const GOOGLE_API_KEY = 'AIzaSyBNtLj7PjM_YoBg6k7ioydVV9EwEc6m8oA';
+const NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/templates/GoogleMiniMap.html'));
+});
+
+app.get('/nearby_search', function(req, api_res) {
+  var radius = req.query.radius? req.query.radius : 150;
+  
+  var params = {
+    'key': GOOGLE_API_KEY,
+    'location': req.query.location,
+    'radius': radius,
+    'type': req.query.type
+  };
+  
+  request({url: NEARBY_SEARCH_URL, qs: params}, function(err, res, body) {
+    if (res.statusCode == 200) {
+      api_res.json(body);
+    } else {
+    
+    }
+
+  });
 });
 
 app.use('/css', express.static('css'));
--- js/GoogleMiniMap.js
+++ js/GoogleMiniMap.js
@@ -1,11 +1,40 @@
+var DEFAULT_LAT = 42.0308;
+var DEFAULT_LNG = -93.6319;
+var GOOGLE_API_KEY = 'AIzaSyBNtLj7PjM_YoBg6k7ioydVV9EwEc6m8oA';
+var DEFAULT_ZOOM = 15;
+var SEARCH_RADIUS = 500;
+
 function initMap() {
-  var Ames = {lat: 42.0308, lng: -93.6319};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: Ames
+  var position = {
+    lat: DEFAULT_LAT,
+    lng: DEFAULT_LNG
+  };
+ 
+  var map = new google.maps.Map($('#map')[0], {
+    zoom: DEFAULT_ZOOM,
+    center: position
   });
+
   var marker = new google.maps.Marker({
-    position: Ames,
+    position: position,
     map: map
+  });
+
+  $.ajax({
+    url: '/nearby_search',
+    data: {
+      'location': position.lat + ',' + position.lng,
+      'type': 'restaurant',
+      'key': GOOGLE_API_KEY,
+      'radius': SEARCH_RADIUS
+    },
+    
+    success: function(data) {
+      debugger;
+    },
+
+    failure: function(data) {
+      debugger;
+    }
   });
 }
