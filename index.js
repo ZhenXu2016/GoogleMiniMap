8b3ca4369c2aad561231ee1d63dd05f5d868cd75
ZhenXu2016 <Zhen.Xu2016@gmail.com>
2017-07-04 02:43:20 +0000

    Add JavaScript Library jquery and underscore

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 
 const PORT_NUMBER = 8080
+const GOOGLE_API_KEY = AIzaSyBNtLj7PjM_YoBg6k7ioydVV9EwEc6m8oA
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/templates/GoogleMiniMap.html'));
