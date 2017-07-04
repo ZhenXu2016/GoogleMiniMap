278bd3304c156222c41adb2e56c94096bd660c5a
ZhenXu2016 <Zhen.Xu2016@gmail.com>
2017-07-04 03:28:03 +0000

    Recreate build_css.sh file

--- build_css.sh
+++ build_css.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#i/bin/bash
 pushd css && for file in *.less; do lessc $file 'basename $file | sed -e "s/less/css/"' ; done && popd
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
 
-const PORT_NUMBER = 8080
-const GOOGLE_API_KEY = AIzaSyBNtLj7PjM_YoBg6k7ioydVV9EwEc6m8oA
+const PORT_NUMBER = 8080;
+const GOOGLE_API_KEY = AIzaSyBNtLj7PjM_YoBg6k7ioydVV9EwEc6m8oA;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/templates/GoogleMiniMap.html'));
-})
+});
 
-app.use('/css', express.static('css'))
-app.use('/js', express.static('js'))
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
 
 app.listen(PORT_NUMBER, function() {
-  console.log('Google Mini Map website listening on port ' + PORT_NUMBER + '!')
-})
+  console.log('Google Mini Map website listening on port ' + PORT_NUMBER + '!');
+});
